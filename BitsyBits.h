9d21c3993cc9b96c6e948eea35bc34476575ff03
vldmkr <vladmakaryan@gmail.com>
2016-03-04 17:47:50 +0200

    initial commit

---
+++ BitsyBits.h
@@ -0,0 +1,10 @@
+#ifndef BITSY_BITS_H
+#define BITSY_BITS_H
+
+#include "TaskScheduler.h"
+#include "ConsoleController.h"
+#include "i2c_adxl345.h"
+#include "i2c_scanner.h"
+#include "i2c_ssd1306.h"
+
+#endif //BITSY_BITS_H
